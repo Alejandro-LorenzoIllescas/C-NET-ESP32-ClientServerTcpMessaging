b61bd4ba77f1c268365aced72ec939342178ae79
alejandroLorenzo <al.lorenzo.illescas@gmail.com>
2024-11-24 19:11:20 +0100

    Implementation for connection with multiple clients

--- ESP32-LocalMessageServer/ESP32-LocalMessageServer.ino
+++ ESP32-LocalMessageServer/ESP32-LocalMessageServer.ino
@@ -5,6 +5,10 @@ const char *password = "123456789";
 
 WiFiServer server(5000); 
 
+// To allow 5 simultanius connections
+const int maxClients = 5;   
+WiFiClient clients[maxClients];  
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Serial print test");
@@ -18,21 +22,57 @@ void setup() {
 }
 
 void loop() {
+
+
+  //  Try to connect new clients
   WiFiClient client = server.available();
+
   if (client) {
-    Serial.println("Client connected");
-    while (client.connected()) {
-      if (client.available()) {
-        Serial.println("Bytes recived by the client");
-        String message = client.readStringUntil('\n');
+    bool added = false;
+    for (int i = 0; i < maxClients; i++) {
+      if (!clients[i] || !clients[i].connected()) {
+        clients[i] = client; // Add the new client to the list
+        Serial.print("New client connected. Assigned to position: ");
+        Serial.println(i);
+        added = true;
+        break;
+      }
+    }
+    if (!added) {
+      // Reject new connection if max clients are reached
+      Serial.println("Max clients reached. Rejecting connection.");
+      client.stop();
+    }
+  }
+
+  //Manage conecctions
+  for (int i = 0; i < maxClients; i++) {
+    if (clients[i] && clients[i].connected()) {
+      if (clients[i].available()) {
+        Serial.print("Data received from client ");
+        Serial.println(i);
+
+        // Read 
+        String message = clients[i].readStringUntil('\n');
         Serial.print("Message: ");
         Serial.println(message);
 
-        client.println("Mesage received: \"" + message + "\"");
-      }
-    }
+        // Send acknowledgment back to the client
+        clients[i].println("Message received: \"" + message + "\"");
 
-    client.stop();
-    Serial.println("Client disconnected");
+        // Send message to all other clients
+        broadcastMessage(message, i);
+
+      } 
+    }
+    //for the momment dont control over the client desconnection
   }
+}
+
+void broadcastMessage(String message, WiFiClient sender) {
+    for (int i = 0; i < maxClients; i++) {
+        if (clients[i] && clients[i] != sender) { // Exclude the sender
+            clients[i].println(message);
+        }
+    }
 }
